5402975fc9351532fa52cc29484615c705657045
ebruaeo <ebruaeo@hotmail.com>
2024-06-23 18:38:23 +0300

    Viewmodel initialized livedata observers

---
+++ app/src/main/java/com/example/learnlivedata/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.learnlivedata
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import com.example.learnlivedata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var myViewModel: MyViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        myViewModel = ViewModelProvider(this).get(MyViewModel::class.java)
+
+        myViewModel.counterLiveData.observe(this, Observer { count ->
+            binding.textViewCounter.text = "$count"
+        })
+
+        binding.buttonIncrement.setOnClickListener {
+            myViewModel.incrementCounter()
+        }
+    }
+}
